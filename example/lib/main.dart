9a5be6631aafd71748c78415c0b6bd4cb6f629c4
MixKage <shishov265@gmail.com>
2024-08-09 17:42:04 +0300

    add: start work on example svg parser

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,123 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(final BuildContext context) => MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(final BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            SvgPicture.asset(
+              'assets/example.svg',
+              width: 1093,
+              height: 761,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // Добавляем область нажатия поверх SVG
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                print('Pressed!');
+              },
+              child: CustomPaint(
+                size: const Size(
+                  300,
+                  300,
+                ),
+                // child: Container(
+                //   color: Colors.green,
+                //   width: 300,
+                //   height: 300,
+                // ),
+                painter: PolygonPainter(),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ),
+  );
+}
+
+class PolygonPainter extends CustomPainter {
+  @override
+  void paint(final Canvas canvas, final Size size) {
+    final paintFill = Paint()
+      ..color = Color(0xFFA04D87) // Цвет заливки
+      ..style = PaintingStyle.fill;
+
+    final paintStroke = Paint()
+      ..color = Colors.black // Цвет обводки
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.0; // Толщина обводки
+
+    // Определение пути из SVG
+    final path = Path()
+      ..moveTo(328.283, 181.09)
+      ..lineTo(328.283, 89.2973)
+      ..lineTo(328.283, 73.4319)
+      ..lineTo(365.964, 73.4319)
+      ..lineTo(365.964, 57.5664)
+      ..lineTo(472.489, 57.5664)
+      ..lineTo(472.489, 237.753)
+      ..lineTo(433.958, 237.753)
+      ..lineTo(433.958, 189.023)
+      ..lineTo(418.093, 189.023)
+      ..lineTo(418.093, 181.09)
+      ..close(); // Закрытие контура
+
+    // Отрисовка пути
+    canvas.drawPath(path, paintFill);
+    canvas.drawPath(path, paintStroke);
+
+    // // Преобразуем размеры в локальные координаты холста
+    // final scaleX = size.width / 200.0;
+    // final scaleY = size.height / 200.0;
+    // final matrix4 = Matrix4.identity()..scale(scaleX, scaleY, 1);
+    // path.transform(matrix4.storage);
+
+    // // Можно настроить стиль рисования для отладки
+    // final paint = Paint()
+    //   ..color = Colors.transparent // Делаем область невидимой
+    //   ..style = PaintingStyle.fill;
+
+    // // Рисуем путь на холсте
+    // canvas.drawPath(path, paint);
+
+    // // Для отладки можно визуализировать границы пути:
+    // final debugPaint = Paint()
+    //   ..color = Colors.red.withOpacity(0.5)
+    //   ..style = PaintingStyle.stroke
+    //   ..strokeWidth = 2.0;
+    // canvas.drawPath(path, debugPaint);
   }
+
+  @override
+  bool shouldRepaint(final CustomPainter oldDelegate) => false;
 }

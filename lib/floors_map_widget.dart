ec97511eff866c2f88b85f49f28280d87b1d4c6c
MixKage <shishov265@gmail.com>
2024-10-18 18:39:30 +0300

    hfix: c->c

--- lib/floors_map_widget.dart
+++ lib/floors_map_widget.dart
@@ -1,4 +1,4 @@
-export 'src/core/config/supported_сlasses.dart';
+export 'src/core/config/supported_classes.dart';
 export 'src/core/failures/parser_error.dart';
 export 'src/core/parser/parser_svg.dart';
 export 'src/core/parser/path_builder.dart';
